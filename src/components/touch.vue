932dd0704f3c5fb299f6d47f21fa35c9e57c0bf6
liuding <liuding@ifmuse.com>
2017-04-17 15:28:51 +0800

    add tap press event

--- src/components/touch.vue
+++ src/components/touch.vue
@@ -1,11 +1,24 @@
-<template>
-	<div class="touch-con" @touchStart="_touchStart" @touchEnd="_touchEnd">
-		<h1>touch</h1>
+<!-- 
+	Touch Component : Only Tap and Press are supported by now . 
+ -->
 
+<template>
+	<div class="touch-con" @touchstart="_touchStart($event)" @touchmove="_touchMove($event)" @touchend="_touchEnd($event)">
+		<div class="touch-child"> 
+
+		</div>
 	</div>
 </template>
 
 <script >
+
+	// 延迟时间 
+	const tapTime = 300 ;
+	const pressTime = 1500 ;
+	// 移动像素
+	const tapDistance = 15 ;
+	const pressDistance = 15 ;
+
 	export default {
 		props : {
 			tap : {
@@ -35,15 +48,58 @@
 		} ,
 		data (){
 			return {
-
+				startX : 0 ,
+				startY : 0 ,
+				touchStartTime : 0 ,
+				touchEndTime : 0 
 			}
 		} ,
 		methods : {
 			_touchStart(ev) {
-				console.log("touchStart");
+				this.startX = ev.targetTouches[0].pageX ;
+				this.startY = ev.targetTouches[0].pageY ;
+				this.touchStartTime = new Date().getTime() ;
+			} ,
+			_touchMove(ev) {
+				let endX = ev.targetTouches[0].pageX ,
+					endY = ev.targetTouches[0].pageY ;
+				console.log("touchMove");
 			} ,
 			_touchEnd(ev) {
+				let endX = ev.changedTouches[0].pageX ,
+					endY = ev.changedTouches[0].pageY ;
+				this.touchEndTime = new Date().getTime() ;
+
+				// tap 
+				if(Math.abs(endX - this.startX) < tapDistance && Math.abs(endY - this.startY) < tapDistance && (this.touchEndTime - this.touchStartTime) < tapTime){
+					this.tapFun();
+					return ;
+				}
+
+				// press 
+				if(Math.abs(endX - this.startX) < pressDistance && Math.abs(endY - this.startY) < pressDistance && (this.touchEndTime - this.touchStartTime) < pressTime){
+					this.pressFun() ;
+					return ;
+				}	
+
+				this.resetTime() ;
 				console.log("touchEnd");
+			} ,
+			resetTime(){
+				this.startX = 0 ;
+				this.startY = 0 ;
+				this.touchStartTime = 0 ;
+				this.touchEndTime = 0 ;
+			} ,
+			tapFun(){
+				//alert("tap");
+				console.log("tap") ;
+				this.tap();
+
+			} ,
+			pressFun(){
+				//alert("press") ;
+				this.press() ;
 			}
 
 		} ,
@@ -58,9 +114,17 @@
 <style scoped>
 
 .touch-con {
-	height: 100px ;
-	border:1px solid #ccc ;
-	background-color: red ;
+	position: relative ;
+	overflow: hidden ;
+}
+
+.touch-child {
+	height: auto ;
+	width: 100% ;
+	/*height: 100px ;
+	background-color: green ;
+	left: 0 ;
+	top: 0 ;*/
 }
 
 </style>
