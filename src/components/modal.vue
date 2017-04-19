86c76007cef3d880263f4454c9d2e634d59579a5
liuding <liuding@ifmuse.com>
2017-04-19 19:54:17 +0800

    plan to build  modal

---
+++ src/components/modal.vue
@@ -0,0 +1,40 @@
+<!-- 
+	Modal
+ -->
+
+ <template>
+ 	<div class="modal-con">
+ 		
+ 	</div>
+ </template>
+
+ <script>
+ 	export default {
+ 		data(){
+ 			return {
+
+ 			}
+ 		} ,
+ 		components : {
+
+ 		} ,
+ 		methods : {
+
+ 			
+
+ 		} ,
+
+ 		mounted(){
+ 			console.log("modal");
+ 		}
+
+ 	}
+
+ </script>
+
+ <style scoped>
+ .modal-con {
+
+ }
+
+ </style>
