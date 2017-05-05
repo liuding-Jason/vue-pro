532346acc697c0bf23d648e4ee9e3d8c5e854db7
liuding <liuding@ifmuse.com>
2017-05-05 16:38:06 +0800

    add navlist

--- src/components/navlist.vue
+++ src/components/navlist.vue
@@ -1,9 +1,10 @@
 <template>
-	<div class="nav-bar flex-h">
+	<div class="nav-bar flex-h topic-border-top">
 	    <div class="nav-bar-item flex1 flex-h flex-hc flex-vc" v-for="item in list">
-	    	<a href="item['hash']">
-	    		<icon></icon>
-	    		<span>{{item['title']}}</span>
+	    	<a v-bind:class="'flex-v flex-vc topic-base-font ' + item['active']" :href="item['hash']" >
+	    		<icon v-if="item['active'] === 'active'" :name="item['actIcon']" :scale="3.2"></icon>
+	    		<icon v-else :name="item['defIcon']" :scale="3.2"></icon>
+	    		<span class="nav-bar-title">{{item['title']}}</span>
 	    	</a>
 	    </div>
   	</div>
@@ -18,32 +19,42 @@
 				list : [{
 					title : '主页' ,
 					alias : 'home' ,
-					hash : '#/home' ,
-					defIcon : '' ,	// 默认状态图标
-					actIcon : ''	// 激活状态图标
+					hash : '/home' ,
+					defIcon : 'home' ,		// 默认状态图标
+					actIcon : 'home-ac',	// 激活状态图标
+					active : location.pathname.indexOf("/home") >= 0 ? 'active' : ''
 				} , {
 					title : '资讯' ,
 					alias : 'consultation' ,
-					hash : '#/Consultation' ,
-					defIcon : '' ,
-					actIcon : ''
+					hash : '/consultation' ,
+					defIcon : 'consultation' ,
+					actIcon : 'consultation-ac' ,
+					active : location.pathname.indexOf("/consultation") >= 0 ? 'active' : ''
 				} , {
-					title : '发现' ,
-					alias : 'find' ,
-					hash  : '#/find' ,
-					defIcon : '' ,
-					actIcon : ''
+					title : '购物车' ,
+					alias : 'shopcar' ,
+					hash  : '/shopcar' ,
+					defIcon : 'shopcar' ,
+					actIcon : 'shopcar-ac' ,
+					active : location.pathname.indexOf("/shopcar") >= 0 ? 'active' : ''
 				} , {
 					title : '个人' ,
 					alias : 'personal' ,
-					hash  : '#/personal' ,
-					defIcon : '' ,
-					actIcon : ''
+					hash  : '/personal' ,
+					defIcon : 'personal' ,
+					actIcon : 'personal-ac' ,
+					active : location.pathname.indexOf("/personal") >= 0 ? 'active' : ''
 				}]
 			}
 		} ,
+		computed : {
+			list : () => {
+				console.log(this.list);
+			}
+		} ,
 		mounted(){
-			console.log("nav");
+			console.log("nav") ;
+			console.log(this.list);
 		}
 	}
 </script>
@@ -56,11 +67,15 @@
   z-index: 100 ;
   height: 2rem ;
   width: 100% ;
-  background-color: red ;
 }
 
 .nav-bar-item {
 
+}
+
+.nav-bar-title{
+	font-size : .4rem ;
+	letter-spacing : .01rem ;
 }
 
 </style>
